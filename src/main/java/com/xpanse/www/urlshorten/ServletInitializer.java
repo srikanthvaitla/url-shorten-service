00131ee7cf79482770904b023043a07187129d1c
svaitla <v-svaitla@expedia.com>
2020-08-08 19:17:13 -0700

    Created initial project setup using start.spring.io

---
+++ src/main/java/com/xpanse/www/urlshorten/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.xpanse.www.urlshorten;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UrlShortenServiceApplication.class);
+	}
+
+}
